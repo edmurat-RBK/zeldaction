f0e52d55b3d96cc0dfb328d76272d3ee49dcb2f6
Edouard MURAT <e.murat@rubika-edu.com>
2020-02-05 16:58:49 +0100

    Added website placeholder

---
+++ Assets/Scripts/TextRandomColor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextRandomColor : MonoBehaviour
+{
+    public int red;
+    public int green;
+    public int blue;
+
+    private int redDirection = 1;
+    private int greenDirection = 1;
+    private int blueDirection = 1;
+
+    private Text textComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0, 500) == 0 || red == 0 || red == 255)
+        {
+            redDirection *= -1;
+        }
+        if (Random.Range(0, 500) == 0 || green == 0 || green == 255)
+        {
+            greenDirection *= -1;
+        }
+        if (Random.Range(0, 500) == 0 || blue == 0 || blue == 255)
+        {
+            blueDirection *= -1;
+        }
+
+        red += redDirection;
+        green += greenDirection;
+        blue += blueDirection;
+
+        textComponent.color = new Color(red / 255.0f, green / 255.0f, blue / 255.0f);
+    }
+}
