46d581f5135ecb171e90d61dd082554b275cdef9
Arthur <arthurgro@outlook.fr>
2020-02-20 11:33:22 +0100

    New default asset Nico pack

---
+++ Assets/Scripts/TEMPLATE/Singletons/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+namespace Game
+{
+    /// <summary>
+    /// General management of the game.
+    /// </summary>
+    public class GameManager : Singleton<GameManager>
+    {
+
+
+
+        void Awake()
+        {
+            MakeSingleton(true);
+        }
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+
+        }
+
+
+
+    }
+}
