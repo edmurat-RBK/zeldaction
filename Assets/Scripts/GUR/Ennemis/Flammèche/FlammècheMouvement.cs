f75cd8af40218d631d6f715c4565b63867ab0f87
Guillaume <g.roge@rubika-edu.com>
2020-03-04 18:29:21 +0100

    Ennemi flammèche
    Changement de direction lors d'une collision et rangement du script

--- Assets/Scripts/GUR/Ennemis/Flammèche/FlammècheMouvement.cs
+++ Assets/Scripts/GUR/Ennemis/Flammèche/FlammècheMouvement.cs
@@ -2,26 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Créateur : Guillaume Rogé 
+/// Ce script permet de :
+/// - faire bouger la flammèche dans 4 directions et ceci de façon aléatoire. 
+/// - Lors d'une collision la flammèche par dans la direction opposé.
+/// - Génerer les flammes derière le passage de la flammèche.
+/// </summary>
+
 public class FlammècheMouvement : MonoBehaviour
 {
+    #region Variable
     [Header("Stat de base")]
+    [Space]
+    public float pv;
     public float speed;
     public float damage;
+
+    [Header ("Comportement de la flamèche")]
+    [Space]
     public float timeBtwDirection;
 
+    [Header ("Prefab de la trainé de flame")]
+    [Space]
     public GameObject flamePrefab;
 
     Vector2 movement;
 
     private int direction;
     private float flameSpawnRate = 0.5f;
+
     private bool lockDirection = true;
     private bool lockGeneration = true;
-   
+    #endregion
+
     void Update()
     {
-        Direction();
         EnnemyMoving();
+        Direction();
         FlameGeneration();
     }
 
@@ -31,7 +49,8 @@ public class FlammècheMouvement : MonoBehaviour
         {
             StartCoroutine(ChangementDirection());
         }
-    }
+    }  // Fonction qui lance la coroutine de changement de direction
+    
     void EnnemyMoving()
     {
         if (direction == 1)
@@ -55,14 +74,14 @@ public class FlammècheMouvement : MonoBehaviour
         }
 
         GetComponent<Rigidbody2D>().velocity = (movement.normalized * speed * Time.fixedDeltaTime);
-    }
+    } // Fonction qui applique le changement de direction
     void FlameGeneration()
     {
         if (lockGeneration == true)
         {
             StartCoroutine(FlameCoolDown());
         }
-    }
+    }      // Fonction qui lance la coroutine de génération de flamme
 
     IEnumerator ChangementDirection()
     {
@@ -70,17 +89,36 @@ public class FlammècheMouvement : MonoBehaviour
         direction = Random.Range(1, 5);
         yield return new WaitForSeconds(timeBtwDirection);
         lockDirection = true;
-    }
+    } // Coroutine qui génére aléatoirement une direction selon un temps donné
     IEnumerator FlameCoolDown()
     {
         lockGeneration = false;
         GameObject flame = Instantiate(flamePrefab, transform.position, transform.rotation);
         yield return new WaitForSeconds(flameSpawnRate);
         lockGeneration = true;
-    }
+    } // Coroutine qui génére des flammes selon un temps donné
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        lockGeneration = true;
-    }
+
+       if (direction == 1)
+       {
+            direction = 2;
+       }
+
+       else if (direction == 2)
+       {
+            direction = 1;
+       }
+
+       else if (direction == 3)
+       {
+            direction = 4;
+       }
+
+       else if (direction == 4)
+       {
+            direction = 3;
+       }
+    } // Detecte la collision et fait partir la flammèche dans le sens opposé
 }
