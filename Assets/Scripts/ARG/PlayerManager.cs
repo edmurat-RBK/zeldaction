b2aaa46931865bcf8bc3c8a5ea5327598577b18b
Arthur <arthurgro@outlook.fr>
2020-03-05 17:13:36 +0100

    Arrosoir programmé sans les collisions
    Arrosoir développer et modification des déplacement du joueur en rajoutant un PlayerManager

---
+++ Assets/Scripts/ARG/PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+namespace Game
+{
+    public class PlayerManager : Singleton<PlayerManager>
+    {
+        #region Variables
+
+        public float horizontal;
+        public float vertical;
+        public bool playerCanMove;
+        public Rigidbody2D playerRigidBody;
+
+        #endregion
+
+        void Awake()
+        {
+            MakeSingleton(true);
+        }
+        
+        void Start()
+        {
+            playerCanMove = true;
+            playerRigidBody = GetComponent<Rigidbody2D>();
+        }
+        
+        void Update()
+        {
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+        }
+        
+        
+        
+    }
+}
