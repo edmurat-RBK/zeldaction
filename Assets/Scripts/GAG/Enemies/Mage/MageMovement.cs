41477954c6ae1d0ab74d5e44c7a3280b61ca41a2
Gobert <g.gobert@rubika-edu.com>
2020-03-09 16:46:54 +0100

    Attaque Mage
    Zone d'apparition météore et cooldown entre chaque attaque

--- Assets/Scripts/GAG/Enemies/Mage/MageMovement.cs
+++ Assets/Scripts/GAG/Enemies/Mage/MageMovement.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Créateur : Gauthier Gobert 
+/// Le script permet de : 
+/// - Déplacer le mage en fonction de la position du joueur
+/// - Faire spawn un projectile sur la position du joueur 
+/// </summary>
+
 public class MageMovement : MonoBehaviour
 {
+    #region Variable
     [Header ("Stats de base")]
     public float pv;
     public float speed;
+    public float timeBeforeAttack;
+    public float attackCooldown;
 
     [Header ("Valeur de déplacement")]
     public float stoppingDistance;
@@ -14,14 +24,14 @@ public class MageMovement : MonoBehaviour
 
     [Space]
     public Transform player;
+    public GameObject projectile;
 
     Vector2 movement;
     Vector2 retreat;
 
-    void Start()
-    {
-   
-    }
+    private bool lockMovement = true;
+    private bool lockAttack = true;
+    #endregion
 
     void Update()
     {
@@ -33,21 +43,44 @@ public class MageMovement : MonoBehaviour
         movement = (player.transform.position - transform.position).normalized;
         retreat = (transform.position - player.transform.position).normalized;
 
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        if (lockMovement == true)
         {
-            GetComponent<Rigidbody2D>().velocity = (movement.normalized * speed * Time.fixedDeltaTime);
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+            {
+                GetComponent<Rigidbody2D>().velocity = (movement.normalized * speed * Time.fixedDeltaTime);
+            }
+
+            else if (Vector2.Distance(transform.position, player.transform.position) < stoppingDistance && Vector2.Distance(transform.position, player.transform.position) > retreatDistance)
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+                if (lockAttack == true)
+                {
+                    StartCoroutine(Attack());
+                }
+            }
+
+            else if (Vector2.Distance(transform.position, player.transform.position) < retreatDistance)
+            {
+                GetComponent<Rigidbody2D>().velocity = (retreat.normalized * speed * Time.fixedDeltaTime);
+            }
         }
+    }  // Fonction qui permet le déplacement du mage
 
-        else if (Vector2.Distance(transform.position, player.transform.position) < stoppingDistance && Vector2.Distance(transform.position, player.transform.position) > retreatDistance)
-        {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        }
+    IEnumerator Attack()
+    {
+        lockMovement = false;
+        yield return new WaitForSeconds(timeBeforeAttack);
+        GameObject meteor = Instantiate(projectile, player.transform.position, transform.rotation);
+        StartCoroutine(Cooldown());
+        lockMovement = true;
 
-        else if (Vector2.Distance(transform.position, player.transform.position) < retreatDistance)
-        {
-            GetComponent<Rigidbody2D>().velocity = (retreat.normalized * speed * Time.fixedDeltaTime);
-        }
+    } // Coroutine qui permet de faire spawn un projectile sur la position du joueur après une incantation
 
-    }
-
+    IEnumerator Cooldown()
+    {
+        lockAttack = false;
+        yield return new WaitForSeconds(attackCooldown);
+        lockAttack = true;
+    } // Coroutine qui gère le cooldown après chaque attaque
 }
