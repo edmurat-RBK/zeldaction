1c48e2e9b4d185cdf923a0c73a61c26dc1f3a5c8
Guillaume <g.roge@rubika-edu.com>
2020-03-10 18:57:27 +0100

    Plaque de pression
    Systeme de base de la plaque de pression

---
+++ Assets/Scripts/GUR/Puzzle/PlaqueDePression.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaqueDePression : MonoBehaviour
+{
+    public bool activeTrap = false;
+
+    public GameObject plaqueActive;
+    public GameObject plaqueDesactive;
+
+    void Start()
+    {
+        plaqueActive.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        plaqueActive.SetActive(true);
+        plaqueDesactive.SetActive(false);
+        activeTrap = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        plaqueActive.SetActive(false);
+        plaqueDesactive.SetActive(true);
+        activeTrap = false;
+    }
+}
