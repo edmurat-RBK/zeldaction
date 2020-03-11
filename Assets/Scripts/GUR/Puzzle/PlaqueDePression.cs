cc611040c4e81dab43c55a4b3179d087141f01c5
Guillaume <g.roge@rubika-edu.com>
2020-03-11 17:25:50 +0100

    Rangement script
    Rangement du script de la plaque de pression

--- Assets/Scripts/GUR/Puzzle/PlaqueDePression.cs
+++ Assets/Scripts/GUR/Puzzle/PlaqueDePression.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Créateur : Guillaume Rogé 
+/// Ce Script permet de : 
+/// - De detecter la hitbox du joueur quand il est sur la plaque et quand il part
+/// - Changer graphiquement la plaque quand elle est activé ou non
+/// - Changer une bool qui permet d'activer les piège relier
+/// </summary>
+
 public class PlaqueDePression : MonoBehaviour
 {
-    public bool activeTrap = false;
-
+    #region Variable
+    [Header ("Glisser les gameobject qui contienne les grph")]
     public GameObject plaqueActive;
     public GameObject plaqueDesactive;
+
+    [HideInInspector]
+    public bool activeTrap = false;
+    #endregion
 
     void Start()
     {
@@ -19,12 +31,12 @@ public class PlaqueDePression : MonoBehaviour
         plaqueActive.SetActive(true);
         plaqueDesactive.SetActive(false);
         activeTrap = true;
-    }
+    } // Permet de changé graphiquement la plaque (activé) et de changé l'etat de la bool en true
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         plaqueActive.SetActive(false);
         plaqueDesactive.SetActive(true);
         activeTrap = false;
-    }
+    } // Permet de changé graphiquement la plaque (désactivé) et de changé l'etat de la bool en false
 }
