10e8456a9b94b5f9e3eccf7eb8e6ded19146ca90
Guillaume <g.roge@rubika-edu.com>
2020-03-12 19:49:51 +0100

    Les base du courant et caisse bois
    Les bases fonctionnes mais encore avec quelques erreurs

---
+++ Assets/Scripts/GUR/CourantEau.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourantEau : MonoBehaviour
+{
+    public float waterSpeed;
+    public string direction;
+
+    Vector2 movement;
+ 
+    void Start()
+    {
+        switch (direction)
+        {
+            case("Bas"):
+                movement = Vector2.down;
+                break;
+
+            case ("Haut"):
+                movement = Vector2.up;
+                break;
+
+            case ("Droite"):
+                movement = Vector2.right;
+                break;
+
+            case ("Gauche"):
+                movement = Vector2.left;
+                break;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 28)
+        {
+            if(other.GetComponent<CubeBois>().canRedirect == true)
+            {
+                other.GetComponent<CubeBois>().canRedirect = false;
+                other.GetComponent<Rigidbody2D>().velocity = (movement.normalized * waterSpeed * Time.fixedDeltaTime);
+                other.GetComponent<CubeBois>().wichDirection = direction;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        other.GetComponent<CubeBois>().canRedirect = true;
+    }
+}
---
+++ Assets/Scripts/GUR/CubeBois.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBois : MonoBehaviour
+{
+    public GameObject hitboxHaut;
+    public GameObject hitboxBas;
+    public GameObject hitboxDroite;
+    public GameObject hitboxGauche;
+
+    public bool canRedirect = true;
+    public GameObject waterSlide;
+    public GameObject teste;
+
+    public string wichDirection;
+
+    private void Start()
+    {
+        hitboxHaut.SetActive(false);
+        hitboxBas.SetActive(false);
+        hitboxDroite.SetActive(false);
+        hitboxGauche.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 29)
+        {
+            switch (wichDirection)
+            {
+                case("Bas"):
+                    waterSlide.SetActive(false);
+                    teste.SetActive(false);
+                    hitboxHaut.SetActive(true);
+                    hitboxBas.SetActive(true);
+
+                    hitboxDroite.SetActive(false);
+                    hitboxGauche.SetActive(false);
+                break;
+
+                case ("Haut"):
+                    waterSlide.SetActive(false);
+                    teste.SetActive(false);
+                    hitboxHaut.SetActive(true);
+                    hitboxBas.SetActive(true);
+
+                    hitboxDroite.SetActive(false);
+                    hitboxGauche.SetActive(false);
+                break;
+
+                case ("Droite"):
+                    waterSlide.SetActive(false);
+                    teste.SetActive(false);
+                    hitboxDroite.SetActive(true);
+                    hitboxGauche.SetActive(true);
+
+                    hitboxHaut.SetActive(false);
+                    hitboxBas.SetActive(false);
+                break;
+
+                case ("Gauche"):
+                    waterSlide.SetActive(false);
+                    teste.SetActive(false);
+                    hitboxDroite.SetActive(true);
+                    hitboxGauche.SetActive(true);
+
+                    hitboxHaut.SetActive(false);
+                    hitboxBas.SetActive(false);
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 29)
+        {
+            waterSlide.SetActive(true);
+            teste.SetActive(true);
+            hitboxHaut.SetActive(false);
+            hitboxBas.SetActive(false);
+            hitboxDroite.SetActive(false);
+            hitboxGauche.SetActive(false);
+        }
+    }
+}
