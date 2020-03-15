541e2f64eba13a13005bf6bf8b58f69597aa8d63
Arthur <arthurgro@outlook.fr>
2020-03-15 18:47:15 +0100

    j'ai oublié

---
+++ Assets/Scripts/ARG/EnnemiDummy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ennemis
+{
+    /// <summary>
+    /// Made by Arthur Galland
+    /// Test for ennemis dammages
+    /// </summary>
+    public class EnnemiDummy : MonoBehaviour
+    {
+        #region Variables
+        public int hitPoint;
+        
+        #endregion
+        
+        void Awake()
+        {
+            
+        }
+        
+        void Start()
+        {
+            hitPoint = 5;
+        }
+        
+        void Update()
+        {
+            Die();
+        }
+        
+        public void EnnemiTakeDammage(int dammage)
+        //fonction used to apply dammages to ennemis use GetComponent<EnnemiDummy>().EnnemiTakeDammage(variable for dammage)
+        {
+            hitPoint -= dammage;
+        }
+
+        private void Die()
+        //fonction used for destroy the ennemis when his hitpoint hit 0
+        {
+            if (hitPoint <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ARG/WaterComportement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Made by Arthur Galland
+    /// Use by the particule system for retrieve the collition object and act on it
+    /// </summary>
+    public class WaterComportement : MonoBehaviour
+    {
+        #region Variables
+        
+        
+        
+        #endregion
+        
+        void Awake()
+        {
+            
+        }
+        
+        void Start()
+        {
+            
+        }
+        
+        void Update()
+        {
+            
+        }
+
+        private void OnParticleCollision(GameObject other)
+        {
+
+            if (other.tag == "Fire")
+            {
+                Destroy(other.gameObject);
+            }
+        }
+
+    }
+}
