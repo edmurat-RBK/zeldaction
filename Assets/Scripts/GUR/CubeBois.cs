70e775ab959723fea82e0940ba7137cfef3f3a56
Guillaume <g.roge@rubika-edu.com>
2020-03-19 19:05:14 +0100

    Caisse et courant finis
    Ajout d'un destructeur et spawner de caisse

--- Assets/Scripts/GUR/CubeBois.cs
+++ Assets/Scripts/GUR/CubeBois.cs
@@ -1,73 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using movementPlayer;
 using Game;
+
+/// <summary>
+/// Créateur : Guillaume Rogé
+/// Ce script permet de : 
+/// - Désactiver les hitbox des courants d'eau quand le joueur va sur une caisse
+/// - Faire spawn d'activer des hitbox quand le joueur est sur une caisse pour l'empécher de tomber dans le courant
+/// - Déplacer le joueur avec la caisse
+/// </summary>
 
 public class CubeBois : MonoBehaviour
 {
-    public GameObject[] courantEau;
-
-    public Transform player;
-
+    #region Variable
+    [Header ("Glisser les hitbox de la caisse")]
     public GameObject hitboxVerticale;
     public GameObject hitboxHorizontale;
 
-    public bool canRedirect = true;
+    [Header ("Mettre la vitesse de déplacement du courant")]
+    public float speedOfWater;
 
     [HideInInspector]
     public string wichDirection;
+    [HideInInspector]
+    public bool canRedirect = true;
+
+    private Vector2 direction;
+    private GameObject[] courantEau;
+    #endregion
 
     private void Start()
     {
-        courantEau = GameObject.FindGameObjectsWithTag("Courant");
-
+        courantEau = GameObject.FindGameObjectsWithTag("Courant"); // Permet de récuperer toutes les hitbox des courants d'eau et de les stocker dans un tableau
         hitboxVerticale.SetActive(false);
         hitboxHorizontale.SetActive(false);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 29)
+        if (collision.gameObject.layer == 29) // 29 = layer Player
         {
-            switch (wichDirection)
+            switch (wichDirection) // Permet de récuperer la direction dans la quel va la caisse et d'activer les bonne hitbox
             {
                 case ("Bas"):
                     hitboxVerticale.SetActive(true);
                     hitboxHorizontale.SetActive(false);
+                    direction = Vector2.down;
                    
                 break;
 
                 case ("Haut"):
                     hitboxVerticale.SetActive(true);
                     hitboxHorizontale.SetActive(false);
-                break;
+                    direction = Vector2.up;
+                    break;
 
                 case ("Droite"):
                     hitboxVerticale.SetActive(false);
                     hitboxHorizontale.SetActive(true);
-                break;
+                    direction = Vector2.right;
+                    break;
 
                 case ("Gauche"):
                     hitboxVerticale.SetActive(false);
                     hitboxHorizontale.SetActive(true);
-                break;
+                    direction = Vector2.left;
+                    break;
             }
 
-            for (int i = 0; i < courantEau.Length; i++)
+            for (int i = 0; i < courantEau.Length; i++) // Permet de désactiver toute les hitbox des courants d'eau 
             {
                 courantEau[i].SetActive(false);
             }
 
-            
+            PlayerManager.Instance.playerRigidBody.AddForce(direction * speedOfWater); // Permet de transporter le joueur sur la caisse
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 29)
+        if (collision.gameObject.layer == 29) // 29 = layer Player
         {
-            for (int i = 0; i < courantEau.Length; i++)
+            for (int i = 0; i < courantEau.Length; i++) // Permet d'activer toute les hitbox des courants d'eau
             {
                 courantEau[i].SetActive(true);
             }
