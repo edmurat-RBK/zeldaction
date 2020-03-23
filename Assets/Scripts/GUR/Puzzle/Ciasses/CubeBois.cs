389f1181b68de43d9621db4cf880b8c05bb94a8a
Arthur <arthurgpro@outlook.fr>
2020-03-23 12:18:39 +0100

    projet rangé
    Nouveau dossiers crées et rangement des préfabs + scripts

---
+++ Assets/Scripts/GUR/Puzzle/Ciasses/CubeBois.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+using Manager;
+
+/// <summary>
+/// Créateur : Guillaume Rogé
+/// Ce script permet de : 
+/// - Désactiver les hitbox des courants d'eau quand le joueur va sur une caisse
+/// - Faire spawn d'activer des hitbox quand le joueur est sur une caisse pour l'empécher de tomber dans le courant
+/// - Déplacer le joueur avec la caisse
+/// </summary>
+
+public class CubeBois : MonoBehaviour
+{
+    #region Variable
+    [Header ("Glisser les hitbox de la caisse")]
+    public GameObject hitboxVerticale;
+    public GameObject hitboxHorizontale;
+
+    [Header ("Mettre la vitesse de déplacement du courant")]
+    public float speedOfWater;
+
+    [HideInInspector]
+    public string wichDirection;
+    [HideInInspector]
+    public bool canRedirect = true;
+
+    private Vector2 direction;
+    private GameObject[] courantEau;
+    #endregion
+
+    private void Start()
+    {
+        courantEau = GameObject.FindGameObjectsWithTag("Courant"); // Permet de récuperer toutes les hitbox des courants d'eau et de les stocker dans un tableau
+        hitboxVerticale.SetActive(false);
+        hitboxHorizontale.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 29) // 29 = layer Player
+        {
+            switch (wichDirection) // Permet de récuperer la direction dans la quel va la caisse et d'activer les bonne hitbox
+            {
+                case ("Bas"):
+                    hitboxVerticale.SetActive(true);
+                    hitboxHorizontale.SetActive(false);
+                    direction = Vector2.down;
+                   
+                break;
+
+                case ("Haut"):
+                    hitboxVerticale.SetActive(true);
+                    hitboxHorizontale.SetActive(false);
+                    direction = Vector2.up;
+                    break;
+
+                case ("Droite"):
+                    hitboxVerticale.SetActive(false);
+                    hitboxHorizontale.SetActive(true);
+                    direction = Vector2.right;
+                    break;
+
+                case ("Gauche"):
+                    hitboxVerticale.SetActive(false);
+                    hitboxHorizontale.SetActive(true);
+                    direction = Vector2.left;
+                    break;
+            }
+
+            for (int i = 0; i < courantEau.Length; i++) // Permet de désactiver toute les hitbox des courants d'eau 
+            {
+                courantEau[i].SetActive(false);
+            }
+
+            PlayerManager.Instance.playerRigidBody.AddForce(direction * speedOfWater); // Permet de transporter le joueur sur la caisse
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 29) // 29 = layer Player
+        {
+            for (int i = 0; i < courantEau.Length; i++) // Permet d'activer toute les hitbox des courants d'eau
+            {
+                courantEau[i].SetActive(true);
+            }
+
+            hitboxVerticale.SetActive(false);
+            hitboxHorizontale.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/GUR/Puzzle/Ciasses/SpawnerCaisse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Créateur : Guillaume Rogé
+/// Ce script permet de : 
+/// - Faire spawn des caisses toute les x seconds
+/// </summary>
+
+public class SpawnerCaisse : MonoBehaviour
+{
+    #region Variable
+    public float spawnRate;
+    public GameObject caissePrefab;
+    private bool canSpawn;
+    #endregion
+    private void Start()
+    {
+        canSpawn = true;
+    }
+    void Update()
+    {
+        if (canSpawn == true)
+        {   
+            StartCoroutine(SpawnCaisse());
+        }
+    }
+
+    IEnumerator SpawnCaisse()
+    {
+        canSpawn = false;
+        Instantiate(caissePrefab, gameObject.transform.position, gameObject.transform.rotation);
+        yield return new WaitForSeconds(spawnRate);
+        canSpawn = true;
+    } // Permet de faire spawn une caisse toute les x seconde
+}
