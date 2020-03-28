bb8d31406eb479e77e6c89d18bf2bc7e14ed6433
Arthur <arthurgpro@outlook.fr>
2020-03-28 11:57:26 +0100

    Cinematic boat and crab
    Creation of 2 cinematics on a new scene
    I alos add the tag "Player" to the collision object of the avatar

---
+++ Assets/Scripts/ARG/Cinematique/CrabCinematique.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Manager;
+
+/// <summary>
+/// Made by Arthur Galland
+/// Script used for activate on a TriggerEnter the crabe cinematique
+/// </summary>
+public class CrabCinematique : MonoBehaviour
+{
+    #region Variable
+    public PlayableDirector crabcin; //reference to the playable cinematique
+    [SerializeField]
+    [Range(1f, 100f)]
+    private float cinematiqueTimer;
+    
+    #endregion
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayCinematique(); //start the cinematique
+            StartCoroutine("MoveAgain"); //player can't move during this cinematique
+            Destroy(this.gameObject); //destroy the collider so the cinematique can't be played again
+        }
+    }
+
+    public void PlayCinematique()
+    {
+        crabcin.Play();
+        PlayerManager.Instance.playerCanMove = false;
+        PlayerManager.Instance.playerRigidBody.velocity = Vector2.zero;
+    }
+
+    IEnumerator MoveAgain()
+    {
+        yield return new WaitForSeconds(cinematiqueTimer);
+        PlayerManager.Instance.playerCanMove = true;
+    }
+}
