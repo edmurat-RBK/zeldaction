c2fee42eade4bf62f13a31eb5d395168da0e6cec
Guillaume <g.roge@rubika-edu.com>
2020-03-28 11:51:58 +0100

    Porte et avancement du donjon

---
+++ Assets/Scripts/GUR/Puzzle/Porte.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porte : MonoBehaviour
+{
+    public float speed;
+    public GameObject[] wichPlaque;
+    public float timeForOpen;
+
+    private Vector2 direction;
+    private Rigidbody2D rbDoor;
+    private int numberOfActive;
+    private bool canOpen;
+    void Start()
+    {
+        rbDoor = gameObject.GetComponent<Rigidbody2D>();
+        canOpen = true;
+        numberOfActive = 0;
+        direction = Vector2.up;   
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < wichPlaque.Length; i++)
+        {
+            if (wichPlaque[i].GetComponent<PlaqueDePression>().activeTrap == true)
+            {
+                numberOfActive += 1;
+            }
+
+            if (numberOfActive == wichPlaque.Length && canOpen == true)
+            {
+                StartCoroutine(OpenDoor());
+            }
+        }
+        numberOfActive = 0;
+    }
+
+    IEnumerator OpenDoor()
+    {
+        canOpen = false;
+        rbDoor.velocity = direction * speed * Time.fixedDeltaTime;
+        yield return new WaitForSeconds(timeForOpen);
+        rbDoor.velocity = Vector2.zero;
+    }
+}
