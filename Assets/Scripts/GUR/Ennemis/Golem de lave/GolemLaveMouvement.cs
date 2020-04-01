1efcab69a1772b5bea233ce1a6c49018894cf927
Guillaume <g.roge@rubika-edu.com>
2020-04-01 17:52:44 +0200

    Golem de lave bientôt finis

--- Assets/Scripts/GUR/Ennemis/Golem de lave/GolemLaveMouvement.cs
+++ Assets/Scripts/GUR/Ennemis/Golem de lave/GolemLaveMouvement.cs
@@ -5,6 +5,7 @@ using Manager;
 
 public class GolemLaveMouvement : MonoBehaviour
 {
+    #region Variable
     [Header("Stats de base")]
     public float pv;
     public float speed;
@@ -15,6 +16,11 @@ public class GolemLaveMouvement : MonoBehaviour
     public float stoppingDistance;
     public float retreatDistance;
 
+    [Header ("Onnde de choc")]
+    public GameObject chocWave;
+
+    public GameObject shotPoint;
+
     Vector2 movement;
 
     private bool lockMouvement;
@@ -22,7 +28,7 @@ public class GolemLaveMouvement : MonoBehaviour
 
     private Transform player;
     private Rigidbody2D rbGolem;
-
+    #endregion
     void Start()
     {
         lockMouvement = true;
@@ -48,16 +54,46 @@ public class GolemLaveMouvement : MonoBehaviour
     {
         movement = (player.transform.position - transform.position).normalized;
 
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        if (lockMouvement == true)
         {
-            rbGolem.velocity = (movement.normalized * speed * Time.fixedDeltaTime);
-        }
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+            {
+                rbGolem.velocity = (movement.normalized * speed * Time.fixedDeltaTime);
+            }
         
-        else if (Vector2.Distance(transform.position, player.transform.position) < stoppingDistance && Vector2.Distance(transform.position, player.transform.position) > retreatDistance)
-        {
-            rbGolem.velocity = Vector2.zero;
+            else if (Vector2.Distance(transform.position, player.transform.position) < stoppingDistance && Vector2.Distance(transform.position, player.transform.position) > retreatDistance)
+            {
+                rbGolem.velocity = Vector2.zero;
+
+                if (lockAttack == true)
+                {
+                    StartCoroutine(GolemLaveAtack());
+                }
+            }
+
         }
     }
 
+    IEnumerator GolemLaveAtack()
+    {
+        lockMouvement = false;
+        yield return new WaitForSeconds(timeBeforeAttack);
 
+        var dir = new Vector2 (player.transform.position.x - shotPoint.transform.position.x, player.transform.position.y - shotPoint.transform.position.y);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        shotPoint.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        GameObject wave = Instantiate(chocWave, shotPoint.transform.position, shotPoint.transform.rotation);
+        Destroy(wave, 0.5f);
+        StartCoroutine(AttackCoolDown());
+        lockMouvement = true;
+    }
+
+    IEnumerator AttackCoolDown()
+    {
+        lockAttack = false;
+        yield return new WaitForSeconds(attackCooldown);
+        lockAttack = true;
+    }
+   
 }
---
+++ Assets/Scripts/GUR/Ennemis/Golem de lave/OndeChoc.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+
+/// <summary>
+///Créateur : Guillaume Rogé
+/// - Ce script permet de : 
+/// - Déplacer l'axe z rotation petit à petit 
+/// - Faire spawn un projectile dans la direction de l'axe z
+/// - Tout cela répéter un nombre de fois par le nombre de projectiles voulu
+/// </summary>
+
+public class OndeChoc : MonoBehaviour
+{
+    #region Variable
+    [Header("Gestion du cône")]
+    public float step;
+    public float start;
+    public int bulNumber;
+   
+    [Header ("Projectile tiré")]
+    public GameObject bulletPrefab;
+
+    [Header ("Vitesse des projectiles")]
+    public float speed;
+
+    Vector2 direction;
+    #endregion
+
+    private void Start()
+    {
+        Fire(); // Lance la fonction de tire
+    }
+
+    void Fire()
+    {
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z - start);
+        
+        for (int i = 0; i < bulNumber; i++)
+        {
+            transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + step);
+            direction = transform.right;
+
+            GameObject bul = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bul.GetComponent<Rigidbody2D>().velocity = direction * speed * Time.fixedDeltaTime;
+
+            Destroy(bul, 3f);
+        }
+
+    } // Fonction qui permet de faire spawn des projectiles selon l'axe z
+}
