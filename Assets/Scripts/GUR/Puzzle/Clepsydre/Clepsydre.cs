d93dbb833c894e8c4bec5d46200abeb60d205ae8
Guillaume <g.roge@rubika-edu.com>
2020-04-05 15:59:15 +0200

    Clepsydre finis

---
+++ Assets/Scripts/GUR/Puzzle/Clepsydre/Clepsydre.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clepsydre : MonoBehaviour
+{
+    public bool actifClepsydre;
+
+    public int valeurParticule;
+    public int maxStockage;
+
+    public float remplissage;
+
+    void Start()
+    {
+        actifClepsydre = false;
+    }
+
+    void Update()
+    {
+        if (remplissage > 0)
+        {
+            actifClepsydre = true;
+            remplissage -= Time.fixedDeltaTime;
+        }
+
+        else if (remplissage <= 0)
+        {
+            actifClepsydre = false;
+        }    
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (remplissage < maxStockage)
+        {
+            Debug.Log("detection trigger");
+            remplissage += valeurParticule;
+        }
+        
+    }
+}
