8f9996c05a17f0bb2dc791e30ae5055f9d8695a1
Edouard Murat <e.murat@rubika-edu.com>
2020-04-20 11:50:59 +0200

    Added PlayerHealth script and added to Player prefab

---
+++ Assets/Scripts/EDM/Player/PlayerHealth.cs
@@ -0,0 +1,67 @@
+﻿using Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that manager health of the player
+/// by Edouard Murat
+/// </summary>
+public class PlayerHealth : MonoBehaviour
+{
+    public float maximumHealth = 3f;
+    public float health = 3f;
+    private bool isDead;
+
+    private PlayerManager playerManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maximumHealth;
+        playerManager = GetComponent<PlayerManager>();
+    }
+
+    // Function that check player vulnerability before taking damage
+    // Called when the avatar is hit by an enemy
+    public void TakeHit(float damage)
+    {
+        if(!playerManager.playerInvulnerable)
+        {
+            TakeDamage(damage);
+        }
+    }
+
+    // Function that give damage to player health
+    // Called in TakeHit()
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+    }
+
+    // Function called at player death
+    // Called in TakeDamage()
+    private void Die()
+    {
+        isDead = true;
+    }
+
+    // Function that restore health
+    // Called when the player use an item that restore health
+    public void Heal(float amount)
+    {
+        if(!isDead)
+        {
+            health += amount;
+            if(health >= maximumHealth)
+            {
+                health = maximumHealth;
+            }
+        }
+    }
+}
