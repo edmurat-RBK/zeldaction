4132f9b2f5617dd27d4b116b145b30ef86d313d4
Guillaume <g.roge@rubika-edu.com>
2020-04-25 12:53:36 +0200

    Pv ennemis presque finit

--- Assets/Scripts/GUR/Ennemis/Gestion Pv Ennemis/PvEnnemis.cs
+++ Assets/Scripts/GUR/Ennemis/Gestion Pv Ennemis/PvEnnemis.cs
@@ -2,42 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Créateur : Guillaume Rogé 
+/// Ce script permet de :
+/// - Detecter la collision du khameo ou des particule d'eau
+/// - En fonction du type d'ennemi soit infliger des dégat ou les rendre vulnérables
+/// - Detruire les ennemis une fois leurs pv à 0
+/// </summary>
+
 public class PvEnnemis : MonoBehaviour
 {
-    public float pv;
-    public float waterForVulnerable;
-    public float timeOfVulnerability;
-
+    #region Variable
+    [Header ("Quel ennemi")]
     public bool flammeche;
     public bool mage;
     public bool golemLave;
 
+    [Header ("Stats")]
+    public float pv;
+    public float waterForVulnerable;
+    public float timeOfVulnerability;
     public float damageParticule;
 
+   [HideInInspector]
     public bool kameoHit;
 
     private float stock;
-    private bool vulnerablePv;
+    #endregion
 
     private void Start()
     {
         stock = waterForVulnerable;
-
-        if (mage == true)
-        {
-            vulnerablePv = gameObject.GetComponent<MageMovement>().vunerable;
-        }
-
-        if (golemLave == true)
-        {
-            vulnerablePv = gameObject.GetComponent<GolemLaveMouvement>();
-        }
     }
 
     void Update()
     {
         KhameoHitFlammeche();
         KhameoHitMage();
+        KhameoHitGolemLave();
         EnnemyDestruction();
 
         if (waterForVulnerable <= 0)
@@ -54,7 +56,12 @@ public class PvEnnemis : MonoBehaviour
             pv -= damageParticule;
         }
 
-        if (mage == true && vulnerablePv == false)
+        if (mage == true && gameObject.GetComponent<MageMovement>().vunerableMage == false)
+        {
+            waterForVulnerable -= damageParticule;
+        }
+
+        if (golemLave == true && gameObject.GetComponent<GolemLaveMouvement>().vunerableGolem == false)
         {
             waterForVulnerable -= damageParticule;
         }
@@ -62,12 +69,12 @@ public class PvEnnemis : MonoBehaviour
 
     void KhameoHitMage()
     {
-        if (mage == true && kameoHit == true && vulnerablePv == false)
+        if (mage == true && kameoHit == true && gameObject.GetComponent<MageMovement>().vunerableMage == false)
         {
             kameoHit = false;
             StartCoroutine(CooldownOfVulnerable());
         }
-    }
+    } // Fonction qui rend vulnérable le mage quand il est touché par le khaméo
 
     void KhameoHitFlammeche()
     {
@@ -75,7 +82,16 @@ public class PvEnnemis : MonoBehaviour
         {
             pv = 0;
         }
-    }
+    }  // Fonction qui set les pv à 0 de la flammèche quand elle se fait toucher par le khaméo
+
+    void KhameoHitGolemLave()
+    {
+        if (golemLave == true && kameoHit == true && gameObject.GetComponent<GolemLaveMouvement>().vunerableGolem == false)
+        {
+            kameoHit = false;
+            StartCoroutine(CooldownOfVulnerable());
+        }
+    } // Fonction qui rend vulnérable le golem de lave quand il est touché par le khaméo
 
     void EnnemyDestruction()
     {
@@ -84,13 +100,22 @@ public class PvEnnemis : MonoBehaviour
             // Anim de mort
             Destroy(gameObject);
         }
-    }
+    } // Fonction qui détruit les ennemis quand leur pv sont à 0 
 
     IEnumerator CooldownOfVulnerable()
     {
-        vulnerablePv = true;
-        yield return new WaitForSeconds(timeOfVulnerability);
-        vulnerablePv = false;
-      
-    }
+        if (mage == true)
+        {
+            gameObject.GetComponent<MageMovement>().vunerableMage = true;
+            yield return new WaitForSeconds(timeOfVulnerability);
+            gameObject.GetComponent<MageMovement>().vunerableMage = false;
+        }
+
+        if (golemLave == true)
+        {
+            gameObject.GetComponent<GolemLaveMouvement>().vunerableGolem = true;
+            yield return new WaitForSeconds(timeOfVulnerability);
+            gameObject.GetComponent<GolemLaveMouvement>().vunerableGolem = false;
+        }
+    } // Coroutine qui rend vulnérable les ennemis pendants x temps
 }
