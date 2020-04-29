ad0359ebd2d1a9502a0aa71363c4b9aa690d1230
Guillaume <g.roge@rubika-edu.com>
2020-04-29 15:05:48 +0200

    Vache presque finit

---
+++ Assets/Scripts/GUR/Puzzle/Vache/CowManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+public class CowManager : Singleton<CowManager>
+{
+    public GameObject[] eachState;
+
+    public int numOfCow;
+
+    public Transform spawnTpCow;
+
+    void Start()
+    {
+        eachState[0].gameObject.SetActive(true);
+        eachState[1].gameObject.SetActive(false);
+        eachState[2].gameObject.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    public void SwitchHitBox()
+    {
+        numOfCow += 1;
+        spawnTpCow.position = new Vector3(spawnTpCow.position.x, (spawnTpCow.position.y + 1), spawnTpCow.position.z);
+
+        if (numOfCow == 1)
+        {
+            eachState[0].gameObject.SetActive(false);
+            eachState[1].gameObject.SetActive(true);
+        }
+
+        if (numOfCow == 2)
+        {
+            eachState[1].gameObject.SetActive(false);
+            eachState[2].gameObject.SetActive(true);
+        }
+    }
+}
