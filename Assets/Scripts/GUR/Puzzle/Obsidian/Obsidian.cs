fb41ceeb68d57d7a2813f386c7c7395d40e4244e
Guillaume <g.roge@rubika-edu.com>
2020-04-29 16:55:42 +0200

    Obsi finit

---
+++ Assets/Scripts/GUR/Puzzle/Obsidian/Obsidian.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsidian : MonoBehaviour
+{
+    public float timeBeforeDestrcution;
+
+    private SpriteRenderer obsiSprite;
+    private BoxCollider2D obsiHitBox;
+
+
+    void Start()
+    {
+
+
+        obsiHitBox = GetComponent<BoxCollider2D>();
+        obsiSprite = GetComponent<SpriteRenderer>();
+        obsiSprite.enabled = false;
+        obsiHitBox.enabled = true;
+    }
+
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        obsiHitBox.enabled = false;
+        obsiSprite.enabled = true;
+
+        StartCoroutine(RespawnObsi());
+    }
+
+    IEnumerator RespawnObsi()
+    {
+        yield return new WaitForSeconds(timeBeforeDestrcution);
+        obsiHitBox.enabled = true;
+        obsiSprite.enabled = false;
+    }
+}
