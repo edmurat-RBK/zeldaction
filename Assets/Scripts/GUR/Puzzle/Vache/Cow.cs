20d58cff64957bf12901871525c536c0e11147b7
Guillaume <g.roge@rubika-edu.com>
2020-04-29 15:05:48 +0200

    Vache presque finit

---
+++ Assets/Scripts/GUR/Puzzle/Vache/Cow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    private bool canActivate;
+    private bool lockCow;
+
+    void Start()
+    {
+        canActivate = false;
+        lockCow = false;
+    }
+
+    
+    void Update()
+    {
+        if (canActivate == true && lockCow == false)
+        {
+            if (Input.GetButton("B"))
+            {
+                gameObject.transform.position = CowManager.Instance.spawnTpCow.position;
+                transform.GetChild(0).gameObject.SetActive(false);
+                CowManager.Instance.SwitchHitBox();
+                lockCow = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 31)
+        {
+            canActivate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 31)
+        {
+            canActivate = false;
+        }
+    }
+}
