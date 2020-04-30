cf7cf6ec77dc7abfbb4d8b4b37cdc07584cd7237
Guillaume <g.roge@rubika-edu.com>
2020-04-30 13:50:07 +0200

    Inté anim clepsydre finit

--- Assets/Scripts/GUR/Puzzle/Clepsydre/Clepsydre.cs
+++ Assets/Scripts/GUR/Puzzle/Clepsydre/Clepsydre.cs
@@ -13,15 +13,21 @@ using UnityEngine;
 
 public class Clepsydre : MonoBehaviour
 {
-    #region
+    #region Variable
     [Header ("Gestion de l'eau de la clepsydre")]
-    public int valeurParticule;
-    public int maxStockage;
+    public float valeurParticule;
+    public float maxStockage;
+
+    [Header ("Pour équilibrer la clepsydre")]
+    public float speedVidage;
+    public float speedRemplissageKhameo;
 
     public float remplissage;
 
+    [Header ("Sprite pour l'anim")]
     public Sprite[] wichSprite;
 
+    private float stateTime;
     private SpriteRenderer clepsydreRenderer;
 
     [HideInInspector]
@@ -37,6 +43,7 @@ public class Clepsydre : MonoBehaviour
 
     void Start()
     {
+        stateTime = maxStockage / 8;
         clepsydreRenderer = GetComponent<SpriteRenderer>();
         lockClepsydre = false;
         actifClepsydre = false;
@@ -47,24 +54,66 @@ public class Clepsydre : MonoBehaviour
     {
         Vidage();
         KhamehoHit();
-
-        switch (remplissage)
-        {
-            default:
-        }
+        GestionVisuel();
+        
     }
+
+    void GestionVisuel()
+    {
+        if (remplissage <= 0)
+        {
+            clepsydreRenderer.sprite = wichSprite[0];
+        }
+
+        else if (remplissage > 0 && remplissage < stateTime)
+        {
+            clepsydreRenderer.sprite = wichSprite[1];
+        }
+
+        else if (remplissage > stateTime && remplissage < (stateTime * 2))
+        {
+            clepsydreRenderer.sprite = wichSprite[2];
+        }
+
+        else if (remplissage > (stateTime * 2) && remplissage < (stateTime * 3))
+        {
+            clepsydreRenderer.sprite = wichSprite[3];
+        }
+
+        else if (remplissage > (stateTime * 3) && remplissage < (stateTime * 4))
+        {
+            clepsydreRenderer.sprite = wichSprite[4];
+        }
+
+        else if (remplissage > (stateTime * 4) && remplissage < (stateTime * 5))
+        {
+            clepsydreRenderer.sprite = wichSprite[5];
+        }
+        
+        else if (remplissage > (stateTime * 5) && remplissage < (stateTime * 6))
+        {
+            clepsydreRenderer.sprite = wichSprite[6];
+        }
+
+        else if (remplissage > (stateTime * 6))
+        {
+            clepsydreRenderer.sprite = wichSprite[7];
+        }
+
+    } // Fonction qui gére l'affiche des sprites en fonction du remplissage
 
     void KhamehoHit()
     {
         if (lockClepsydre == false)
         {
-            if (clepsydreHit == true)
+            if (clepsydreHit == true && remplissage < maxStockage)
             {
-                remplissage = maxStockage;
+                remplissage += Time.fixedDeltaTime * speedRemplissageKhameo;
                 clepsydreHit = false;
             }
         }
-    }
+    } // fonction qui gére le remplissage de la clepsydre quand la khaméo le touche
+
     void Vidage()
     {
         if (lockClepsydre == false)
@@ -72,13 +121,12 @@ public class Clepsydre : MonoBehaviour
             if (remplissage > 0)
             {
                 actifClepsydre = true;
-                remplissage -= Time.fixedDeltaTime;
+                remplissage -= Time.fixedDeltaTime * speedVidage;
             }
 
             else if (remplissage <= 0)
             {
                 actifClepsydre = false;
-                remplissage = 0
             }    
         }
     } // Fonction qui s'occupe de soustraire la valeur de remplissage par le temps qui passe
