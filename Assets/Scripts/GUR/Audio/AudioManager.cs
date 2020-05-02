679834279fe6dc9e0bb3026f802e985e93ad95e6
Guillaume <g.roge@rubika-edu.com>
2020-05-02 18:13:30 +0200

    Audio Manager finit

---
+++ Assets/Scripts/GUR/Audio/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play(""); // Pour musique commence au début
+    }
+
+
+    public void Play (string name)
+    {
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Le nom du song n'est pas écrit correctement");
+            return;
+        }
+        s.source.Play();
+    }
+    
+}
