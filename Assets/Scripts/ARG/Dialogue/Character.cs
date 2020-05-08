f72e657fe1e0d7c374799949cee79e35dfd62cf8
Arthur <arthurgpro@outlook.fr>
2020-05-08 14:16:52 +0200

    Début de dialogue UI + Clean Boss

---
+++ Assets/Scripts/ARG/Dialogue/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName ="Dialogue/Character")]
+public class Character : ScriptableObject
+{
+    public string nameOfCharacter;
+    public Sprite sprite;
+}
