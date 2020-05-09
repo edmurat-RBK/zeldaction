2808b6781ba65e43fe519c22604f76bb516af763
Guillaume <g.roge@rubika-edu.com>
2020-05-09 13:49:52 +0200

    HitBox finit

---
+++ Assets/Scripts/GUR/Exeption/TpPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpPlayer : MonoBehaviour
+{
+    public Transform tpPoint;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = tpPoint.transform.position;
+        }
+    }
+
+}
