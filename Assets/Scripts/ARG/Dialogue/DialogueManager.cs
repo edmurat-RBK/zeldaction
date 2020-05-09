bfd6f1e550382573f9cdf2e0e5f0efd2216c528c
Arthur <arthurgpro@outlook.fr>
2020-05-09 14:26:48 +0200

    Commit sans la cinématique

---
+++ Assets/Scripts/ARG/Dialogue/DialogueManager.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using Management;
+
+namespace Dialogue
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        //[SerializeField] private InputManager _input = null;
+
+        [Header("Dialogue")]
+        public Conversation _actualConversation;
+        private Phrase _actualPhrase;
+        private int _PhraseCounter = 0;
+        private int _SentanceCounter = 0;
+
+        [Header("UI Elements")]
+        [SerializeField] private RectTransform _textBox = null;
+        [SerializeField] private Image _Sprite = null;
+        [SerializeField] private Text _nom = null;
+        [SerializeField] private Text _dialogues = null;
+
+        [Header("Variable")]
+        [SerializeField] [Range(0, 0.05f)]
+        private float DelayBetweenLetters = 0.05f;
+        [SerializeField]
+        private float tempsLectParLettre = 0.1f;
+        public bool haveEnd = false;
+        public bool isAuto = false;
+
+        void Start()
+        {
+            //_input = GameObject.FindGameObjectWithTag("GameController").GetComponent<InputManager>();
+        }
+
+        public void BeginCoversation(Conversation conversation)
+        {
+            _actualConversation = conversation;
+
+            _PhraseCounter = 0;
+
+            _actualPhrase = _actualConversation.phraseList[_PhraseCounter];
+
+            _SentanceCounter = 0;
+
+            haveEnd = false;
+
+            StartCoroutine(StartDialogue());
+        }
+
+        private IEnumerator StartDialogue()
+        {
+            float enterTime = 0.3f;
+
+            LeanTween.moveY(_textBox, -150, enterTime);
+
+            yield return new WaitForSeconds(enterTime);
+           
+            NextEchange();
+        }
+
+        private IEnumerator TypeSentence(string name, string phrase)
+        {
+            _dialogues.text = "";
+            _nom.text = "";
+
+            _Sprite.sprite = _actualPhrase.interlocuteur;
+            _nom.text = name;
+
+
+            foreach (char letter in phrase.ToCharArray())
+            {
+                _dialogues.text += letter;
+
+                yield return new WaitForSeconds(DelayBetweenLetters);
+
+            }
+
+
+            float wait = phrase.Length * tempsLectParLettre;
+
+            if (!Input.GetButtonDown("X"))
+            {
+                yield return new WaitForSeconds(wait);
+            }
+
+            yield return new WaitForSeconds(0.1f);
+
+            DisplayNextSentence();
+        }
+
+        public void DisplayNextSentence()
+        {
+            if (_SentanceCounter >= _actualPhrase._phrase.Length)
+            {
+                if (isAuto)
+                {
+                    NextEchange();
+                }
+                return;
+            }
+
+            if (haveEnd)
+            {
+                //StartDialogue();
+                haveEnd = false;
+
+            }
+
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(_actualPhrase.nameInterlocuteur, _actualPhrase._phrase[_SentanceCounter]));
+            
+            _SentanceCounter++;
+
+
+        }
+
+        public void NextEchange()
+        {
+            if (_PhraseCounter >= _actualConversation.phraseList.Length)
+            {
+                EndDialogue();
+                return;
+            }
+
+            StopAllCoroutines();
+
+            _actualPhrase = _actualConversation.phraseList[_PhraseCounter];
+            _SentanceCounter = 0;
+
+            DisplayNextSentence();
+
+            _PhraseCounter++;
+
+        }
+
+        public void EndDialogue()
+        {
+            StopAllCoroutines();
+            _dialogues.text = "";
+            _nom.text = "";
+            _Sprite.sprite = null;
+            LeanTween.moveY(_textBox, -300, 0.3f);
+            haveEnd = true;
+
+        }
+    }
+}
---
+++ Assets/Scripts/ARG/Dialogue/NextDialogue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Dialogue
+{
+    public class NextDialogue : MonoBehaviour
+    {
+        [Header("Component")]
+        private DialogueManager dialogManag;
+
+        void OnEnable()
+        {
+            dialogManag = GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>();
+            dialogManag.isAuto = false;
+            dialogManag.NextEchange();
+            
+        }
+    }
+}
