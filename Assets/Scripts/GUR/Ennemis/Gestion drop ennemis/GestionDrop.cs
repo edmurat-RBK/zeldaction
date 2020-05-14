132c5a970be7f4c3e8f7e9cd0c9a2bd04aae16e8
Guillaume <g.roge@rubika-edu.com>
2020-05-14 18:28:46 +0200

    potion de vie

---
+++ Assets/Scripts/GUR/Ennemis/Gestion drop ennemis/GestionDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionDrop : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GUR/PickUp/HealthPotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().health += 1;
+            collision.gameObject.GetComponent<HealthBar>().HealthSysteme();
+            Destroy(gameObject);
+        }
+    }
+}
