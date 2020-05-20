e1d473f02b4cd965555eabae0adb55151fa90513
Guillaume <g.roge@rubika-edu.com>
2020-05-20 12:35:06 +0200

    UI Fontaine finit

---
+++ Assets/Scripts/GUR/UI/Player/Fontaine/FontaineUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+using UnityEngine.UI;
+
+public class FontaineUI : MonoBehaviour
+{
+    public Slider slider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = PlayerManager.Instance.maxCooldownF;
+        slider.value = PlayerManager.Instance.cooldownF;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = PlayerManager.Instance.maxCooldownF - PlayerManager.Instance.cooldownF;
+    }
+}
