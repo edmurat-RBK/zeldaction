2e31f686137f7dd335a18f4d5046bfc0d74af819
Guillaume <g.roge@rubika-edu.com>
2020-05-21 12:44:20 +0200

    Donjon Chaman Save

---
+++ Assets/Scripts/GUR/Save/ForceDoorOpen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceDoorOpen : MonoBehaviour
+{
+    public enum lieu
+    {
+        DonjonChaman,
+        Vache,
+        Milieu,
+        Volcan,
+        DonjonLave
+    }
+
+    public lieu where;
+
+    void Start()
+    {
+        if (where == lieu.DonjonChaman)
+        {
+            if (UpgradesManager.List["finishShaman"] == true)
+            {
+                GetComponent<GestionActivateur>().whoActivate.RemoveAll(list_item => list_item != null);
+            }
+        }
+    }
+}
