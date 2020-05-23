72f68df14a07ef80064db53b64dcab04fdd289e8
Youmna Sahab <youmnasahab@gmail.com>
2020-05-23 17:40:46 +0200

    prep la timeline
    fix animator cinematique

---
+++ Assets/DeactivateAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    public Animator BossCinematique;
+    public Animator BossManager;
+
+    void Start()
+    {
+               
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopAnimator()
+    {
+        BossCinematique.enabled=false;
+        BossManager.enabled=true;
+    }
+}
