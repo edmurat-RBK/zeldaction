0be74c47618ddfd3aba8575bbd423d3ae34cd435
Youmna Sahab <youmnasahab@gmail.com>
2020-05-23 21:53:28 +0200

    cendre ok timeline...préparée pr les gd?

---
+++ Assets/Scripts/YOS/TriggerCendre.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCendre : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public  ParticleSystem cendre;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D colid)
+    {
+        if (colid.gameObject.tag == "Player")
+        {
+            cendre.Play();
+        }
+    }
+}
+    
