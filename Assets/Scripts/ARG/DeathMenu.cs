1408f98e9eb39d257a2c4de8da2c2faddc2f28ef
Arthur <arthurgpro@outlook.fr>
2020-05-25 11:13:03 +0200

    DeathScreen PlaceHodlder

---
+++ Assets/Scripts/ARG/DeathMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Manager;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        PlayerManager.Instance.GetComponent<PlayerHealth>().respawn();
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
