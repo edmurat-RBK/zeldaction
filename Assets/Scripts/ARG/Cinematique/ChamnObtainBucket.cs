433f1ead24f0a7a3d8a87fec52ee7074d9c96d7a
Arthur <arthurgpro@outlook.fr>
2020-05-25 11:51:41 +0200

    Obtain Bucket by Chaman with Dialogue

---
+++ Assets/Scripts/ARG/Cinematique/ChamnObtainBucket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+
+public class ChamnObtainBucket : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && UpgradesManager.List["as bucket"] == false)
+        {
+            if (Input.GetButtonDown("X"))
+            {
+                collision.GetComponent<PlayerManager>().getBucket = true;
+                collision.GetComponent<PlayerManager>().obtainBucket();
+                GetComponent<UpgradeObject>().Gotcha();
+            }
+        }
+    }
+}
