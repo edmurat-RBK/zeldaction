1fac5c4617e92bbe3da1ee4eff7babea529de5a1
Guillaume <g.roge@rubika-edu.com>
2020-05-27 11:54:15 +0200

    Début Fade

---
+++ Assets/Scripts/GUR/UI/Fade/FadeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+public class FadeManager : Singleton<CowManager>
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    void FadeIn()
+    {
+
+    }
+}
