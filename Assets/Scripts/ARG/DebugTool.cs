9746915dc476d610322d62b4ea81e845c7545280
Guillaume <g.roge@rubika-edu.com>
2020-05-30 16:30:08 +0200

    DebugTool inv good

---
+++ Assets/Scripts/ARG/DebugTool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+
+public class DebugTool : MonoBehaviour
+{
+    private bool isInvinsible = false;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (isInvinsible == false)
+            {
+
+                isInvinsible = true;
+                PlayerManager.Instance.playerInvulnerable = true;
+            }
+            else
+            {
+                isInvinsible = false;
+                PlayerManager.Instance.playerInvulnerable = false;
+            }
+        }
+    }
+}
