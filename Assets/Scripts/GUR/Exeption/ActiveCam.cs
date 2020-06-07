30ba059e18c4e9594bff23c03ca41d9abbb77fb6
Guillaume <g.roge@rubika-edu.com>
2020-06-07 12:41:22 +0200

    fix cam boss

--- Assets/Scripts/GUR/Exeption/ActiveCam.cs
+++ Assets/Scripts/GUR/Exeption/ActiveCam.cs
@@ -7,14 +7,12 @@ public class ActiveCam : MonoBehaviour
 {
     public CinemachineVirtualCamera cam;
 
-
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             cam.Priority += 5;
+            collision.gameObject.transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
