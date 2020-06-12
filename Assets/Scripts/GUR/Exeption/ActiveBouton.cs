cd2d8b37fb80ca8ae60724a36a9946343315d3f7
Guillaume <g.roge@rubika-edu.com>
2020-06-12 18:43:47 +0200

    Ajout bouton

---
+++ Assets/Scripts/GUR/Exeption/ActiveBouton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveBouton : MonoBehaviour
+{
+    public GameObject ui;
+
+    private void Start()
+    {
+        ui.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ui.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            ui.SetActive(false);
+        }
+    }
+}
